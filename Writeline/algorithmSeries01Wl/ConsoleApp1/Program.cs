bfa4226e325fb10b93495dde34706e692036c92e
krisis-code <menesbaspinar@hotmail.com>
2023-05-17 10:11:14 +0300

    WriteLineMethod
    WriteLine metodunu write metodu kullanarak writeLine metodu takliti yapan bir metod yazdım

---
+++ Writeline/algorithmSeries01Wl/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+
+        public void MyCustomWrite(string text)
+        {
+            Console.Write(text); // Write ön tanımlı metodunu kullanarak oluşturduğumuz string değeri ekrana yazdırıyoruz
+            Console.Write('\n'); // Write ön tanımlı metodunu kullanarak alt satıra geçmek için \n değerini giriyoruz
+
+        }
+        static void Main(string[] args)
+        {
+            string text = "Bu bir örnek";
+
+            Program program = new Program(); // Program sınıfının bir örneğini oluşturun
+            program.MyCustomWrite(text); // Örneği kullanarak metodu çağırın
+            program.MyCustomWrite(text);
+        }
+
+
+    }
+}
