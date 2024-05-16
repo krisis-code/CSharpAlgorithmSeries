8365beec3a2d648e9a87d640db51e0477b95a68b
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-16 19:36:12 +0300

    İlk algoritma 2 sayıtoplama algoritması yazıldı

---
+++ CSharpAlgorithmSeries01/Program.cs
@@ -0,0 +1,14 @@
+﻿
+/* Ekrandan girilen 2 adet sayının toplanmını bulan algoritma */
+int a, b, sum = 0;
+
+Console.WriteLine("1. Sayı giriniz");
+
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("2. Sayı giriniz");
+b = Convert.ToInt32(Console.ReadLine());
+
+sum = a + b;
+
+Console.WriteLine("İki sayının toplamı = {0}", sum);
