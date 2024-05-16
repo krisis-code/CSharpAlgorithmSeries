d614ea6e67229fbb65a57bbf82e3f3a3057cf9b6
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-16 19:52:11 +0300

    İki sayının , Toplamını ,çarpımını bölümünü ve farkını bulan algoritma

--- CSharpAlgorithmSeries01/Program.cs
+++ CSharpAlgorithmSeries01/Program.cs
@@ -1,14 +1,29 @@
 ﻿
-/* Ekrandan girilen 2 adet sayının toplanmını bulan algoritma */
+/* Ekrandan girilen 2 adet sayının toplanmını bulan algoritma 
+
+
 int a, b, sum = 0;
+Console.WriteLine("1. Sayı giriniz");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("2. Sayı giriniz");
+b = Convert.ToInt32(Console.ReadLine());
+sum = a + b;
+Console.WriteLine("İki sayının toplamı = {0}", sum);
+*/
+
+/*İki sayının , Toplamını ,çarpımını bölümünü ve farkını bulan algoritma
+
+float a, b, sum ,dvd,imp,diff= 0;
 
 Console.WriteLine("1. Sayı giriniz");
-
 a = Convert.ToInt32(Console.ReadLine());
-
 Console.WriteLine("2. Sayı giriniz");
 b = Convert.ToInt32(Console.ReadLine());
 
 sum = a + b;
+dvd = a / b;
+imp = a * b;
+diff = a - b;
 
-Console.WriteLine("İki sayının toplamı = {0}", sum);
+Console.WriteLine("Toplam = {0} \nÇarpım = {1} \nBölüm = {2} \nFark = {3}", sum, imp, dvd, diff);
+ */
