edd6f15edeaf07b2490cf6efa407d554cfa4d41d
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-16 19:57:40 +0300

    Vize ve finale göre ortalama bulan algoritma

--- CSharpAlgorithmSeries01/Program.cs
+++ CSharpAlgorithmSeries01/Program.cs
@@ -27,3 +27,18 @@ diff = a - b;
 
 Console.WriteLine("Toplam = {0} \nÇarpım = {1} \nBölüm = {2} \nFark = {3}", sum, imp, dvd, diff);
  */
+
+/*Vize ve finale göre ortalama bulan algoritma
+
+double Vize, Final, sum  = 0;
+
+Console.WriteLine("Vize puanınızı giriniz");
+Vize = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Final puanınızı giriniz");
+Final = Convert.ToInt32(Console.ReadLine());
+
+sum = (Vize * (0.40)) + (Final * (0.80));
+
+Console.WriteLine("Ortalamanız {0}", sum);
+
+*/
