de5a5668774cba6d8c5428157cf944886a4b6f07
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-05-23 22:47:11 +0300

    Revize

--- CSharpAlgorithmSeries01/Program.cs
+++ CSharpAlgorithmSeries01/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-/* Ekrandan girilen 2 adet sayının toplanmını bulan algoritma 
+/* Ekrandan girilen 2 adet sayının toplanmını bulan algoritma */
 
 
 int a, b, sum = 0;
@@ -9,5 +9,5 @@ Console.WriteLine("2. Sayı giriniz");
 b = Convert.ToInt32(Console.ReadLine());
 sum = a + b;
 Console.WriteLine("İki sayının toplamı = {0}", sum);
-*/
+
 
