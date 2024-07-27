ff370c838bc56861aa8a50daf68c15eb92adadcc
Muhammed Enes Başpınar <Menesbaspinar@hotmail.com>
2024-07-27 21:06:58 +0300

    Arrays

--- CSharpAlgorithmSeries01/Program.cs
+++ CSharpAlgorithmSeries01/Program.cs
@@ -1,22 +1,54 @@
-﻿
-//For
-for (int i = 0; i < 5; i++)
-{
-    Console.WriteLine("For İterasyon: " + i);
-}
+﻿int[] sayilar = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-//While
-int j = 0;
-while (j < 5)
-{
-    Console.WriteLine("While İterasyon :" +j);
-   j++;
-}
-
-//Foreach
-int[] sayilar = { 1, 2, 3, 4, 5 };
 foreach (int sayi in sayilar)
 {
-    Console.WriteLine("Foreach İterasyon: " + sayi);
+    if (sayi % 2 == 0)
+    {
+        Console.WriteLine("Çift Sayı: " + sayi);
+    }
 }
 
+char[,] kare = { { '*', '*', '*', '*', '*' }, { '*', '*', '*', '*', '*' }, { '*', '*', '*', '*', '*' }, { '*', '*', '*', '*', '*' }, { '*', '*', '*', '*', '*' } };
+
+int lengt = kare.GetLength(0);
+
+for (int i = 0; i <lengt; i++)
+{
+    for (int j = 0; j < lengt; j++)
+    {
+        Console.Write(kare[i, j] + " ");
+    }
+    Console.WriteLine();
+
+
+}
+
+//int ucgen = 5;
+
+//for (int i = 0; i <= ucgen; i++)
+//{
+//    for (int j = 0; j <= i; j++)
+//    {
+//        Console.Write("*");
+//    }
+//    Console.WriteLine();
+//}
+int satirSayisi = 10; // Üçgenin yüksekliği
+
+for (int i = 1; i <= satirSayisi; i++)
+{
+    // Her satır için gerekli boşlukları yazdır
+    for (int j = 0; j < (satirSayisi - i); j++)
+    {
+        Console.Write(" ");
+    }
+
+    // '+' karakterlerini yazdır
+    for (int k = 0; k < (2 * i - 1); k++)
+    {
+        Console.Write("+");
+    }
+
+    // Satır sonu ve yeni satıra geçiş
+    Console.WriteLine();
+}
